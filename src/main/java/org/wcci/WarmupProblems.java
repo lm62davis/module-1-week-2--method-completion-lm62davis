3a0b2990e2fca4c418669c99ecbee1e61f286650
Linda Martinez Davis <74939495+lm62davis@users.noreply.github.com>
2020-11-27 19:57:21 -0500

    Update WarmupProblems.java
    Added Problem #2 - Icy Hot.

--- src/main/java/org/wcci/WarmupProblems.java
+++ src/main/java/org/wcci/WarmupProblems.java
@@ -42,3 +42,27 @@ public class WarmupProblems {
     }
 
 }
+import java.util.Arrays;
+import java.util.Random;
+
+public class IcyHot {
+    public static void main(String[] args) {
+        // Return true if one is less than 0 and the other is greater than 100
+        int[] temps = {-1, 2, 120};
+        Random randTemp1 = new Random();
+        Random randTemp2 = new Random();
+        int strTemp1 = randTemp1.nextInt(temps.length);
+        int strTemp2 = randTemp2.nextInt(temps.length);
+       if (strTemp1 < 0) {
+            System.out.println(strTemp1);
+        } else if (strTemp1 > 100); {
+            System.out.println(strTemp1);
+        }
+        if (strTemp2 < 0) {
+            System.out.println(strTemp2);
+        } else if (strTemp2 > 100); {
+            System.out.println(strTemp2);
+        }
+        System.out.println(Arrays.toString(temps));
+    }
+}
